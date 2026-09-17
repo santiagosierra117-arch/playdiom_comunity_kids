f46f2b9f7df28fb5ba5651dc7117bbe773c6d3d8
santiagosierra117-arch <santiagosierra117a@gmail.com>
2026-09-17 11:30:55 -0500

    Add buildItActivity with exercises and hints

--- data/activities.js
+++ data/activities.js
@@ -1,1 +1,33 @@
+const buildItActivity = {
+  type: "build",
+  id: "to-be-001",
 
+  title: "Build It",
+  instruction: "Toca las palabras y forma la oración correcta.",
+
+  hint: {
+    title: "HELP",
+    formula: "PRONOUN + TO BE + COMPLEMENT",
+    example: "She + is + a girl"
+  },
+
+  starsPerExercise: 3,
+
+  exercises: [
+    {
+      sentence: ["She", "is", "a", "girl"]
+    },
+    {
+      sentence: ["He", "is", "a", "boy"]
+    },
+    {
+      sentence: ["I", "am", "a", "boy"]
+    },
+    {
+      sentence: ["I", "am", "a", "girl"]
+    },
+    {
+      sentence: ["They", "are", "friends"]
+    }
+  ]
+};
